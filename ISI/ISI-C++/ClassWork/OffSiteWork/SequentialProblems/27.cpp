892d9637ff25f1887c849e51ca2a0af569dbf5cf
joseph-emmanuel <joseph.emmanuel@outlook.com>
2019-11-23 05:12:56 -0500

    First commit from srface after completing 27 c++ program

--- ISI/ISI-C++/ClassWork/OffSiteWork/SequentialProblems/27.cpp
+++ ISI/ISI-C++/ClassWork/OffSiteWork/SequentialProblems/27.cpp
@@ -1,30 +1,60 @@
 #include <iostream>
 using namespace std;
 float taxCalulator(bool, bool, int, float);
+float AdditionalTaxRed(float);
 int main()
 {
     bool IsMarried, IsNew;
     int NoofChildren;
     float BasicSalary, TotalTax;
-    cout << " Is the person is married(true/false)";
+    cout << " Is the person is married(true/false) : ";
     cin >> IsMarried;
-    cout << " Is the person is new to the province(true/false)";
+    cout << " Is the person is new to the province(true/false) : ";
     cin >> IsNew;
-    cout << "Enter the number of childern";
+    cout << "Enter the number of childern : ";
     cin >> NoofChildren;
-    cout << "Enter the salary";
+    cout << "Enter the salary : ";
     cin >> BasicSalary;
-    if (BasicSalary > 0 && BasicSalary < 18000)
-    {
-        cout << taxCalulator(IsMarried, IsNew, NoofChildren, BasicSalary);
-    }
+    cout <<"The total tax amount after all reduction is : "<< taxCalulator(IsMarried, IsNew, NoofChildren, BasicSalary);
 }
-float taxCalulator(bool IsMarried, bool IsNew, int NoofChildren, float BasicSalary);
+float taxCalulator(bool IsMarried, bool IsNew, int NoofChildren, float BasicSalary)
 {
-    bool IsMarried, IsNew;
-    int NoofChildren;
-    float BasicSalary, totalTax;
-    totalTax = BasicSalary if (IsMarried)
+    
+    float  totalTax,taxRate=0;
+    totalTax = AdditionalTaxRed(BasicSalary) ;
+    if (IsMarried)
     {
+       taxRate=taxRate+.02;
     }
+    if (IsNew)
+    {
+        taxRate=taxRate+.08;
+    }
+    if (NoofChildren > 0)
+    {        
+        taxRate=taxRate+(NoofChildren*.005);
+    }
+    totalTax=totalTax*(1-taxRate);
+    return totalTax;
+}
+float AdditionalTaxRed(float basicSalary)
+{
+    float totalTax;
+    if (basicSalary >= 0 && basicSalary <= 18000)
+    {
+        totalTax=basicSalary*0.10;
+    }
+    else if (basicSalary >= 18000.01 && basicSalary <= 32000)
+    {
+        totalTax=(18000*0.10)+((basicSalary-18000)*0.20);
+    }
+    else if (basicSalary >= 32000.01 && basicSalary <= 60000)
+    {
+        totalTax=((basicSalary-32000)*0.30)+(14000*0.20)+(18000*0.10);
+    }
+    else
+    {
+        totalTax=((basicSalary-60000)*40)+(28000*0.3)+(14000*0.20)+(18000*0.10);
+    }
+    return totalTax;
 }
