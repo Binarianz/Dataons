e601dbf847dbf7fb8e2b3bb535d64149970f8179
joseph-emmanuel <joseph.emmanuel@outlook.com>
2019-11-26 04:23:23 -0500

    added functions four functions and three declarations

--- ISI/ISI-C++/Project/Project.cpp
+++ ISI/ISI-C++/Project/Project.cpp
@@ -2,12 +2,51 @@
 #include<fstream>
 #include<string>
 #include<time.h>
-
 using namespace std;
+string line;
+
+int validationInput(int);
+void printUnderScore(int);
+int lineLength();
+void stringComp(string);
 int main() 
 {
-	string line;
-	int randomNumber, rLine = 0,stringLength;
+	int guess=7;
+	printUnderScore(lineLength());
+	while (guess != 0)
+	{
+		stringComp(line);
+	}
+	system("pause");
+	
+	return 0;
+}
+
+int validationInput(int value)
+{
+	//int value;
+	while (cin.fail() || cin.peek() != '\n')
+	{
+		cin.clear();
+		cin.ignore(512, '\n');
+		cout << "Warning - please enter a valid value !: ";
+		cin >> value;
+	}
+	return value;
+}
+
+void printUnderScore(int number)
+{
+	for (int i = 0; i < number; i++)
+	{
+		cout << " _ ";
+	}
+	cout << endl;
+}
+int lineLength()
+{
+	//string line;
+	int randomNumber, rLine = 0, stringLength = 0;
 	srand(time(NULL));
 	randomNumber = rand() % 20;
 	ifstream myfile("words.txt");
@@ -16,16 +55,39 @@ int main()
 		while (getline(myfile, line))
 		{
 			++rLine;
-			if(randomNumber==rLine){
-				cout << line << '\n';
-			stringLength = line.size();
-			cout << stringLength << '\n';
+			if (randomNumber == rLine) {
+				return line.size();
 			}
 		}
 		myfile.close();
+
 	}
 
 	else cout << "Unable to open file";
-	system("pause");
-	return 0;
+}
+void stringComp(string a)
+{
+	int i,j=0;
+	int length = a.size();
+	char guess;
+	char* p=new char[length] ;
+	
+	cout << "\n Enter your Guess ; ";
+	cin >> guess;
+	guess = validationInput(guess);
+	for ( i = 0; i < length; i++)
+	{
+		if (line[i]==guess)
+		{
+			p[i] = a[i];
+			j++;
+		}
+		else
+		{
+			p[i] = '_';
+			j++;
+		}
+		cout << p;
+	}
+	delete[] p;
 }
