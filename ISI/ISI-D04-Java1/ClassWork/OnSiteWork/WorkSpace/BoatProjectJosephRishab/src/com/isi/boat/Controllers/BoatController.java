f5b890f01aafcb4df324e61680c2c109f341a8de
joseph-emmanuel <joseph.emmanuel@outlook.com>
2020-03-02 22:06:22 -0500

    trying to revert the commits done

--- ISI/ISI-D04-Java1/ClassWork/OnSiteWork/WorkSpace/BoatProjectJosephRishab/src/com/isi/boat/Controllers/BoatController.java
+++ ISI/ISI-D04-Java1/ClassWork/OnSiteWork/WorkSpace/BoatProjectJosephRishab/src/com/isi/boat/Controllers/BoatController.java
@@ -1,5 +1,16 @@
 package com.isi.boat.Controllers;
 
+import com.isi.boat.Models.BoatModel;
+import com.isi.boat.Views.MasterPanel;
+	
 public class BoatController {
 
+	private BoatModel model; // Model
+	private MasterPanel window; // View
+	
+	public BoatController(BoatModel model,MasterPanel window)
+	{
+		this.model=model;
+		this.window=window;
+	}
 }
